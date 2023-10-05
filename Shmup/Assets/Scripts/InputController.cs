10de5bb37e00883c5bbe19983a1fefb6fe7236a2
Jason <c04jason@gmail.com>
2023-10-04 22:11:11 -0400

    Added keybindings with placeholder debug text

--- Shmup/Assets/Scripts/InputController.cs
+++ Shmup/Assets/Scripts/InputController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,5 +12,30 @@ public class InputController : MonoBehaviour
     public void OnMove(InputAction.CallbackContext context)
     {
         controller.SetDirection(context.ReadValue<Vector2>());
+    }
+
+    public void OnMouseClick(InputAction.CallbackContext context)
+    {
+        context.action.performed += ctx =>
+        {
+            if (Mouse.current.leftButton.isPressed)
+            {
+                Debug.Log("Left Mouse Click");
+            }
+            else if (Mouse.current.rightButton.isPressed)
+            {
+                Debug.Log("Right Mouse Click");
+            }
+        };
+    }
+
+    public void OnDash(InputAction.CallbackContext context)
+    {
+        Debug.Log("Dash");
+    }
+
+    public void OnSheild(InputAction.CallbackContext context)
+    {
+        Debug.Log("Sheild");
     }
 }
