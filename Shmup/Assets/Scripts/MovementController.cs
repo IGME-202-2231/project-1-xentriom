703c2b673eb63be0f52fb7860aca1440d5825f42
Jason <c04jason@gmail.com>
2023-10-05 00:56:49 -0400

    Changed it to only face left/right, added temp player

--- Shmup/Assets/Scripts/MovementController.cs
+++ Shmup/Assets/Scripts/MovementController.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
+    [SerializeField] float speed = 5.0f;
+    SpriteRenderer spriteRenderer;
     Camera cam;
     float camHeight;
     float camWidth;
 
     Vector3 objectPosition = Vector3.zero;
-
-    [SerializeField] float speed = 5.0f;
-
     Vector3 direction = Vector3.zero;
     Vector3 velocity = Vector3.zero;
 
@@ -23,6 +22,9 @@ public class MovementController : MonoBehaviour
         camWidth = camHeight * cam.aspect;
 
         objectPosition = transform.position;
+        // Get the SpriteRenderer component of the current game object
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
     }
 
     // Update is called once per frame
@@ -34,7 +36,7 @@ public class MovementController : MonoBehaviour
         transform.position = objectPosition;
 
         // Clamp the x value to be between -camWidth/2 and 0, which are the edges of the left half of the screen
-        objectPosition.x = Mathf.Clamp(objectPosition.x, -camWidth / 2f + .5f, 0f - (camWidth / 2 * .20f));
+        objectPosition.x = Mathf.Clamp(objectPosition.x, -camWidth / 2f + .1f, 0f - (camWidth / 2 * .20f));
 
         // Assign the clamped object position to the object's transform
         transform.position = objectPosition;
@@ -58,9 +60,10 @@ public class MovementController : MonoBehaviour
         {
             direction = newDirection.normalized;
 
-            if (direction != Vector3.zero)
+            // Flip the sprite only if the current direction is different from the previous direction and not zero
+            if ((direction.x < 0f) != spriteRenderer.flipX && direction.x != 0f)
             {
-                transform.rotation = Quaternion.LookRotation(Vector3.back, direction);
+                spriteRenderer.flipX = direction.x < 0f;
             }
         }
     }
